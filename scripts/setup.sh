6810be69f8b202a1f27b7efaaf2dba4294cd9623
El Hudson <ehudson@wellesley.edu>
2023-08-16 19:39:03 +0000

    ugh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,1 @@
-export NOTION_DB="0e2c8717f10341a2a2d30f48eb2c6677"
-export NOTION_TOKEN="secret_oPnTR8UwwPWeDeJReXH7rZwRb2ILunhXd35AQOn2xmY"
-export PROJECT_PATH=$PWD
-flask --app HUBRIS run --debug & npx webpack --watch && fg
+npx webpack --watch & flask --app HUBRIS run --debug && fg
