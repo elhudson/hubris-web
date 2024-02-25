6b18446e5566ab7e14335d1ffe1798e377b13c66
El Hudson <ehudson@wellesley.edu>
2024-02-25 18:34:04 -0500

    bop

--- components/link.jsx
+++ components/link.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
 
 export default ({ feature, table }) => {
-  return <Link to={`/srd/${table}/${feature?.id}`}>{feature?.title}</Link>;
+  return <Link reloadDocument to={`/srd/${table}/${feature?.id}`}>{feature?.title}</Link>;
 };
