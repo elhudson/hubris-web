a13c96ccfd848f61380c1a9211a66cbd9b8bee48
El Hudson <ehudson@wellesley.edu>
2024-03-03 20:48:23 -0500

    top level await!

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    supported: {
+      'top-level-await': true //browsers can handle top-level-await features
+    },
+  },
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
